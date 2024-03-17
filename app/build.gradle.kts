5c1afbf5f0491274bf5e5d2faf2be347c8f5e6ed
devhyper <57877914+devhyper@users.noreply.github.com>
2024-03-17 13:37:30 -0700

    Version 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.devhyper.openvideoeditor"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1"
+        versionCode = 5
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
